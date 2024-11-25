066b5c1b84b6c8b2a8aa3e9774fec737a670ee79
KuroeProg <itsumy81@gmail.com>
2024-11-25 11:44:31 +0100

    mini changes

--- server.c
+++ server.c
@@ -6,40 +6,50 @@
 /*   By: cfiachet <cfiachet@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 17:09:29 by cfiachet          #+#    #+#             */
-/*   Updated: 2024/11/23 17:47:00 by cfiachet         ###   ########.fr       */
+/*   Updated: 2024/11/25 11:44:10 by cfiachet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void    ft_displaynbr(pid_t pid)
+int ft_checkpid(pid_t pid, argv[1])
 {
-    if (pid > 9)
-    {
-        ft_displaynbr(pid / 10);
-        ft_displaynbr(pid % 10);
-    }
-    else
-    {
-        pid = pid + '0';
-        write(1, &pid, 1);
-    }
+    if (pid == argv[1])
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
 
-void    get_pid()
+
+void	ft_displaynbr(pid_t pid)
 {
-    pid_t pid;
-
-    pid = getpid();
-    if (pid != -1)
-    {
-        ft_displaynbr(pid);
-    }
-
+	if (pid > 9)
+	{
+		ft_displaynbr(pid / 10);
+		ft_displaynbr(pid % 10);
+	}
+	else
+	{
+		pid = pid + '0';
+		write(1, &pid, 1);
+	}
 }
 
-int main(void)
+void	get_pid()
 {
-    get_pid();
-    return 0;
+	pid_t pid;
+
+	pid = getpid();
+	if (pid != -1)
+	{
+		ft_displaynbr(pid);
+	}
+}
+
+int	main(void)
+{
+	get_pid();
+	return (0);
 }
