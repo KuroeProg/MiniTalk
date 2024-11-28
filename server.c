e0a5590178e038cccb3ae11458a30e9dfbe6d1ed
KuroeProg <itsumy81@gmail.com>
2024-11-28 18:20:16 +0100

    server.c

--- server.c
+++ server.c
@@ -6,36 +6,41 @@
 /*   By: cfiachet <cfiachet@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 17:09:29 by cfiachet          #+#    #+#             */
-/*   Updated: 2024/11/28 18:01:53 by cfiachet         ###   ########.fr       */
+/*   Updated: 2024/11/28 18:19:47 by cfiachet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h>
 
-int	signal_handler(int signal, siginfo_t *info, void *str)
+void	signal_handler(int signal, siginfo_t *info, void *str)
 {
 	static int	i;
 	static int	bit;
 
-	(void)info;
 	(void)str;
-
+	if (signal == SIGUSR1)
+		i |= (0x01 << bit);
+	if (bit == 8)
+	{
+		bit = 0;
+		printf("%c", i);
+		i = 0;
+		kill(info->si_pid, SIGUSR2);
+	}
 }
-
 
 int	main(int argc, char **argv)
 {
+	struct sigaction	signal;
+
 	if (argc != 1)
 		return (printf("Error, number of arguments invalid.\n"), 0);
-	struct sigaction	s_sa;
-
-	s_sa.sa_sigaction = &signal_handler;
-	s_sa.sa_flags = 0;
-	sigemptyset(&s_sa.sa_mask);
-
-	sigaction(SIGUSR1, &s_sa, NULL);
-	sigaction(SIGUSR2, &s_sa, NULL);
+	signal.sa_sigaction = signal_handler;
+	signal.sa_flags = 0;
+	sigemptyset(&signal.sa_mask);
+	sigaction(SIGUSR1, &signal, NULL);
+	sigaction(SIGUSR2, &signal, NULL);
 	printf("Hello! The PID is : %d\nWaiting for a message\n", getpid());
 	while (1)
 		pause();
