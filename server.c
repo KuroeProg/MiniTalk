7bd5b7fabf39b4fe3ca64b4df216a66d0df1676d
KuroeProg <itsumy81@gmail.com>
2024-11-29 18:44:09 +0100

    server.c edit for getting the entire sentence

--- server.c
+++ server.c
@@ -6,26 +6,65 @@
 /*   By: cfiachet <cfiachet@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 17:09:29 by cfiachet          #+#    #+#             */
-/*   Updated: 2024/11/28 18:21:24 by cfiachet         ###   ########.fr       */
+/*   Updated: 2024/11/29 18:43:45 by cfiachet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 #include <stdio.h>
 
-void	signal_handler(int signal, siginfo_t *info, void *str)
+void	reset(t_str *str)
 {
-	static int	i;
-	static int	bit;
+	free(str->str);
+	str->str = NULL;
+	str->i = 0;
+}
 
-	(void)str;
-	if (signal == SIGUSR1)
-		i |= (0x01 << bit);
-	if (bit == 8)
+void	add_char(t_str *str, char c)
+{
+	char	*new;
+	int		size;
+
+	size = str->i++;
+	new = malloc(sizeof(int) * size + 1);
+	if (!new)
 	{
-		bit = 0;
-		printf("%c", i);
-		i = 0;
+		reset(str);
+		exit(EXIT_FAILURE);
+	}
+	if (str->str)
+	{
+		while(str->i--)
+			new[str->i] = str->str[str->i];
+		free(str->str);
+	}
+	new[size - 1] = c;
+	new[size] = '\0';
+	str->str = new;
+	str->i = size;
+}
+
+void	signal_handler(int signal, siginfo_t *info, void *s)
+{
+	static char		c;
+	static t_str 	str;
+
+	(void)s;
+	c = 0;
+	if (signal == SIGUSR1)
+		c |= (0x01 << str.bit);
+	str.bit++;
+	if (str.bit == 8)
+	{
+		str.bit = 0;
+		if (c == '\0')
+		{
+			printf("%s\n", str.str);
+			reset(&str);
+		}
+		else
+			add_char(&str, i);
+		c = 0;
 		kill(info->si_pid, SIGUSR2);
 	}
 }
@@ -37,7 +76,7 @@ int	main(int argc, char **argv)
 	(void)argv;
 	if (argc != 1)
 		return (printf("Error, number of arguments invalid.\n"), 0);
-	signal.sa_sigaction = signal_handler;
+	signal.sa_sEsigaction = signal_handler;
 	signal.sa_flags = 0;
 	sigemptyset(&signal.sa_mask);
 	sigaction(SIGUSR1, &signal, NULL);
